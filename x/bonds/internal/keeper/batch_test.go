7f194cbb12b02e8f42172075448cd0680c684cd1
Miguel Dingli <migueldingli97@gmail.com>
2020-08-26 11:14:27 +0200

    Update x/bonds/internal/keeper/batch_test.go

--- x/bonds/internal/keeper/batch_test.go
+++ x/bonds/internal/keeper/batch_test.go
@@ -1039,7 +1039,7 @@ func TestCancelUnfulfillableBuys(t *testing.T) {
 
 	buyPrices := sdk.DecCoins{sdk.NewInt64DecCoin(reserveToken, 100)}
 	maxPrices := sdk.Coins{sdk.NewInt64Coin(reserveToken, 1100)}
-	blankSellPrices := sdk.NewDecCoins(nil...) // blank
+	blankSellPrices := sdk.NewDecCoinsFromCoins(nil) // blank
 	zeroTokens := sdk.NewCoin(bond.Token, sdk.ZeroInt())
 
 	testCases := []struct {
